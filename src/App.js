990c1435fc29b0a334ae821896f2028ba1bdbe27
souvik-xavi <souvikghata2@gmail.com>
2022-06-22 18:02:34 +0530

    fate intialisation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { React, useState } from "react";
+import "./App.css";
+import GameLayout from "./Components/GameLayout";
 
 function App() {
+  const [rows, setRows] = useState(6);
+  const [columns, setColumns] = useState(5);
+  const [player1, setPlayer1] = useState("player1");
+  const [player2, setPlayer2] = useState("player2");
+  var [display, setDisplay] = useState(true);
+  function handlerow(e) {
+    setRows(e.target.value);
+  }
+  function handlecolumn(e) {
+    setColumns(e.target.value);
+  }
+  function handleplayer1(e) {
+    setPlayer1(e.target.value);
+  }
+
+  function handleplayer2(e) {
+    setPlayer2(e.target.value);
+  }
+  const handleDisplay = () => {
+    display = setDisplay(!display);
+    console.log(display);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {display ? (
+        <div>
+          Rows:
+          <input type="number" value={rows} min="6" onChange={handlerow} />
+          {rows}
+          <br />
+          Columns:
+          <input
+            type="number"
+            value={columns}
+            min="5"
+            onChange={handlecolumn}
+          />
+          {columns}<br />
+          Player1 :
+          <input
+            type="text"
+            placeholder="player name"
+            onChange={handleplayer1}
+          /> {player1}<br />
+          Player 2:
+           <input
+            type="text"
+            placeholder="player name"
+            onChange={handleplayer2}
+          /> {player2}<br />
+
+          <button onClick={handleDisplay}> Generate Grid</button>
+        </div>
+      ) : (
+        <GameLayout rows={rows} columns={columns} player1={player1} player2={player2}/>
+      )}
     </div>
   );
 }
---
+++ src/Components/GameLayout.js
@@ -0,0 +1,258 @@
+import React, { useState } from "react";
+
+function GameLayout(props) {
+  var [arrayLayout, setArrayLayout] = useState([[]]);
+  var [flag, setflag] = useState(false);
+
+  const [color, setColor] = useState("red");
+  function GridHead(r, c) {
+    var row1 = [];
+    var arr1 = [];
+    for (let i = 0; i < r; i++) {
+      row1.push(
+        <div
+          style={{
+            display: "flex",
+            marginTop: "35%",
+            justifyContent: "center",
+          }}
+        >
+          <span
+            style={{
+              border: "2px solid black",
+              height: "25px",
+              width: "25px",
+              borderRadius: "50%",
+              backgroundColor: color,
+            }}
+          ></span>
+        </div>
+      );
+    }
+    for (let i = 0; i < c; i++) {
+      arr1.push(row1);
+    }
+    return arr1;
+  }
+  var arr = new Array(props.columns).fill(0).map(() =>
+    new Array(props.rows).fill(
+      <div
+        style={{
+          display: "flex",
+          marginTop: "35%",
+          justifyContent: "center",
+        }}
+      >
+        <span
+          style={{
+            border: "2px solid black",
+            height: "25px",
+            width: "25px",
+            borderRadius: "50%",
+            backgroundColor: "white",
+          }}
+        ></span>
+      </div>
+    )
+  );
+  console.log(props.player1)
+  console.log(props.player2)
+
+  const b = GridHead(props.rows, 1);
+  const setGrid = () => {
+    setArrayLayout(arr);
+    setflag(false);
+  };
+  const handleColor = (e, key) => {
+    let copy = [...arrayLayout];
+    var emp = props.columns - 1;
+    console.log(copy[emp][key].props.children.props.style.backgroundColor);
+    while (
+      emp >= 0 &&
+      copy[emp][key].props.children.props.style.backgroundColor != "white"
+    ) {
+      emp = emp - 1;
+      console.log(emp);
+    }
+    if (emp < 0) {
+      alert("The grid is Over");
+    }
+    if (color == "red") {
+      copy[emp][key] = (
+        <div
+          style={{
+            display: "flex",
+            marginTop: "35%",
+            justifyContent: "center",
+          }}
+        >
+          <span
+            style={{
+              border: "2px solid black",
+              height: "25px",
+              width: "25px",
+              borderRadius: "50%",
+              backgroundColor: "red",
+            }}
+          ></span>
+        </div>
+      );
+      setArrayLayout(copy);
+      setColor("yellow");
+    } else if (color == "yellow") {
+      copy[emp][key] = (
+        <div
+          style={{
+            display: "flex",
+            marginTop: "35%",
+            justifyContent: "center",
+          }}
+        >
+          <span
+            style={{
+              border: "2px solid black",
+              height: "25px",
+              width: "25px",
+              borderRadius: "50%",
+              backgroundColor: "yellow",
+            }}
+          ></span>
+        </div>
+      );
+      setArrayLayout(copy);
+      setColor("red");
+    }
+
+    if (
+      (copy[emp][key].props.children.props.style.backgroundColor ===
+        copy[emp][key - 1].props.children.props.style.backgroundColor &&
+        copy[emp][key - 1].props.children.props.style.backgroundColor ===
+          copy[emp][key - 2].props.children.props.style.backgroundColor &&
+        copy[emp][key - 2].props.children.props.style.backgroundColor ===
+          copy[emp][key - 3].props.children.props.style.backgroundColor) ||
+      (copy[emp][key].props.children.props.style.backgroundColor ===
+        copy[emp][key - 1].props.children.props.style.backgroundColor &&
+        copy[emp][key + 1].props.children.props.style.backgroundColor ===
+          copy[emp][key + 2].props.children.props.style.backgroundColor &&
+        copy[emp][key + 2].props.children.props.style.backgroundColor ===
+          copy[emp][key + 3].props.children.props.style.backgroundColor &&
+        copy[emp][key].props.children.props.style.backgroundColor !== "white" &&
+        copy[emp][key - 1].props.children.props.style.backgroundColor ===
+          "white")
+    ) {
+      setflag(true);
+    } else if (
+      (copy[emp][key].props.children.props.style.backgroundColor ===
+        copy[emp + 1][key].props.children.props.style.backgroundColor &&
+        copy[emp + 1][key].props.children.props.style.backgroundColor ===
+          copy[emp + 2][key].props.children.props.style.backgroundColor &&
+        copy[emp + 2][key].props.children.props.style.backgroundColor ===
+          copy[emp + 3][key].props.children.props.style.backgroundColor) ||
+      (copy[emp][key].props.children.props.style.backgroundColor ===
+        copy[emp - 1][key].props.children.props.style.backgroundColor &&
+        copy[emp - 1][key].props.children.props.style.backgroundColor ===
+          copy[emp - 2][key].props.children.props.style.backgroundColor &&
+        copy[emp - 2][key].props.children.props.style.backgroundColor ===
+          copy[emp - 3][key].props.children.props.style.backgroundColor &&
+        copy[emp - 2][key].props.children.props.style.backgroundColor !==
+          "white" &&
+        copy[emp - 3][key].props.children.props.style.backgroundColor ===
+          "white")
+    ) {
+        setflag(true);
+    } else if (
+      (copy[emp][key].props.children.props.style.backgroundColor ===
+        copy[emp + 1][key+1].props.children.props.style.backgroundColor &&
+        copy[emp + 1][key+1].props.children.props.style.backgroundColor ===
+          copy[emp + 2][key +2 ].props.children.props.style.backgroundColor &&
+        copy[emp + 2][key +2].props.children.props.style.backgroundColor ===
+          copy[emp + 3][key +3 ].props.children.props.style.backgroundColor) ||
+      (copy[emp][key].props.children.props.style.backgroundColor ===
+        copy[emp - 1][key -1 ].props.children.props.style.backgroundColor &&
+        copy[emp - 1][key -1].props.children.props.style.backgroundColor ===
+          copy[emp - 2][key -2].props.children.props.style.backgroundColor &&
+        copy[emp - 2][key -2].props.children.props.style.backgroundColor ===
+          copy[emp - 3][key - 3].props.children.props.style.backgroundColor &&
+        copy[emp - 2][key -2].props.children.props.style.backgroundColor !==
+          "white" &&
+        copy[emp - 3][key - 3].props.children.props.style.backgroundColor ===
+          "white")
+    ) {
+        setflag(true);
+    }
+    else if (
+        (copy[emp][key].props.children.props.style.backgroundColor ===
+          copy[emp + 1][key-1].props.children.props.style.backgroundColor &&
+          copy[emp + 1][key-1].props.children.props.style.backgroundColor ===
+            copy[emp + 2][key - 2 ].props.children.props.style.backgroundColor &&
+          copy[emp + 2][key - 2].props.children.props.style.backgroundColor ===
+            copy[emp + 3][key - 3 ].props.children.props.style.backgroundColor) ||
+        (copy[emp][key].props.children.props.style.backgroundColor ===
+          copy[emp - 1][key+ 1 ].props.children.props.style.backgroundColor &&
+          copy[emp - 1][key +1].props.children.props.style.backgroundColor ===
+            copy[emp - 2][key + 2].props.children.props.style.backgroundColor &&
+          copy[emp - 2][key + 2].props.children.props.style.backgroundColor ===
+            copy[emp - 3][key + 3].props.children.props.style.backgroundColor &&
+          copy[emp - 2][key +2].props.children.props.style.backgroundColor !==
+            "white" &&
+          copy[emp - 3][key +2].props.children.props.style.backgroundColor ===
+            "white")
+      ) {
+        setflag(true);
+      }
+
+
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={(e) => setGrid()}>Get Grid</button>
+        {b.map((items, index) => {
+          return (
+
+            <div style={{ display: "flex" }}>
+              {items.map((subItems, sIndex) => {
+                return (
+                  <div
+                    style={{
+                      border: "2px solid black",
+                      height: "100px",
+                      width: "100px",
+                    }}
+                    key={sIndex}
+                    onClick={(e) => handleColor(e, sIndex)}
+                  >
+                    {subItems}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+
+      {flag? color==='red'? `${props.player2} wins`:`${props.player2} wins` :arrayLayout.map((items, index) => {
+        return (
+          <div style={{ display: "flex" }}>
+            {items.map((subItems, sIndex) => {
+              return (
+                <div
+                  style={{
+                    border: "2px solid black",
+                    height: "100px",
+                    width: "100px",
+                  }}
+                >
+                  {subItems}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default GameLayout;
